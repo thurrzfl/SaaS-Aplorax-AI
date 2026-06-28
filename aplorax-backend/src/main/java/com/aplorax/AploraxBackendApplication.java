e91f37ce9bbb3b396d3d253a1590d806a1105c19
thurrzfl <arthurpaulo1979@gmail.com>
2026-06-28 19:30:13 -0300

    projeto spring boot inicial com a conexa ao postgresql pronta

---
+++ aplorax-backend/src/main/java/com/aplorax/AploraxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aplorax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AploraxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AploraxBackendApplication.class, args);
+	}
+
+}
---
+++ aplorax-backend/src/test/java/com/aplorax/AploraxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplorax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AploraxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
